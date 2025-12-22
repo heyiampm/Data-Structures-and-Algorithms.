2116c4bd3e5decbd4d39c6470da544db7e0b5870
Pm Pranta <heyiampm@gmail.com>
2025-12-22 19:21:52 +0600

    DSA

---
+++ Ternary Operator/Operator1.cpp
@@ -0,0 +1,10 @@
+//Positive Negative find.
+
+#include<iostream>
+using namespace std;
+int main(){
+          int n = 45;
+
+          cout <<( n >= 0 ? "Positive" : "Negative");
+          return 0;
+}
