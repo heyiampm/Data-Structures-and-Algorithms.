9c96fdff3ca08ff4cdd6801c0cd33959d15cee77
Pm Pranta <heyiampm@gmail.com>
2025-12-22 19:34:58 +0600

    DSA

---
+++ Loop/Demo2.cpp
@@ -0,0 +1,13 @@
+// print N value loop.//1 to 20.
+
+#include<iostream>
+using namespace std;
+int main(){
+          int n = 20;
+          int i = 1;
+          while( i <= n){
+                    cout<< i << " ";
+                    i++;
+          }
+          return 0;
+}
---
+++ Loop/Demo3.cpp
@@ -0,0 +1,13 @@
+// For loop
+
+#include<iostream>
+using namespace std;
+int main(){
+          int n = 20;
+          int i = 1;
+          while( i <= n){
+                    cout<< i << " ";
+                    i++;
+          }
+          return 0;
+}
