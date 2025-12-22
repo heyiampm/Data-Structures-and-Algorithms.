68992473619d001459848fc9e73cc3b2969885a1
Pm Pranta <heyiampm@gmail.com>
2025-12-22 19:33:52 +0600

    DSA

---
+++ Ascii value print/Ascii.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+          int grade = 'A';    //Asci value print process.
+          int value = grade;  //this process is convert value.
+          cout<< grade ;
+          return 0;
+}
