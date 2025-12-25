9a73d9079263226999c13a5ae4f67f74eeaeca20
Pm Pranta <heyiampm@gmail.com>
2025-12-25 12:03:35 +0600

    DSA

---
+++ Loop/Demo4.cpp
@@ -0,0 +1,11 @@
+// For loop
+
+#include<iostream>
+using namespace std;
+int main(){
+          int n = 3;
+          for(int i = 1; i <=3; i++){
+                    cout<< i<<" ";
+          }
+          return 0;
+}
---
+++ Loop/Demo5.cpp
@@ -0,0 +1,28 @@
+//
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+          
+//           do{
+//                     cout<<"Pranta Mohajan";
+
+//           } while(2>5);
+//           return 0;
+// }
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+          int n = 10;
+          int i = 1;
+          
+          do{
+                    cout<< i <<" ";
+                    i++;
+          } while(i <=n );
+          cout << endl;
+          return 0;
+}
