bd1906b404b5019c60a948ed3280784b193245ec
Pm Pranta <heyiampm@gmail.com>
2025-12-25 12:05:35 +0600

    DSA

---
+++ Nested loop/No1.cpp
@@ -0,0 +1,10 @@
+//📌 10 star print in using nested loop.
+
+#include<iostream>
+using namespace std;
+int main () {
+          for (int i = 1; i<=10; i++){
+                    cout <<"*****"<< endl;
+                    }
+          return 0;
+}
---
+++ Nested loop/No2.cpp
@@ -0,0 +1,15 @@
+//📌 5 star print in using nested loop.
+
+#include<iostream>
+using namespace std;
+int main (){
+          for(int i =1; i<=5; i++) {
+                    int m = 5;
+                    for (int j = 1; j<=m; j++){
+                              cout<<"*";
+                    }
+                    cout<<endl;
+          }
+          
+          return 0;
+}
