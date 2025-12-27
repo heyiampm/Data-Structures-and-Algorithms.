70c26b2fb82c795a2e319560b138c94ed25d326b
Pm Pranta <heyiampm@gmail.com>
2025-12-27 12:00:47 +0600

    DSA

---
+++ Unary Operators/Unary.cpp
@@ -0,0 +1,21 @@
+/*📌 Increment Decrement*/
+#include<iostream>  
+using namespace std;
+int main(){
+          int a = 10;
+          int b = a++;    //post increment.(work first, update 2nd)
+          cout<<"b is = " <<b <<endl;
+          cout<<"a is = " <<a;
+          return 0;
+}
+
+/*📌 Increment Decrement*/
+#include<iostream>  
+using namespace std;
+int main(){
+          int a = 10;
+          int b = ++a;    //pre increment.( update,work)
+          cout<<"b is = " <<b <<endl;
+          cout<<"a is = " <<a;
+          return 0;
+}
---
+++ Unary Operators/Unary2.cpp
@@ -0,0 +1,21 @@
+/*📌 Increment Decrement*/
+#include<iostream>  
+using namespace std;
+int main(){
+          int a = 10;
+          int b = --a;    //per decrement.
+          cout<<"b is = " <<b <<endl;
+          cout<<"a is = " <<a;
+          return 0;
+}
+
+/*📌 Increment Decrement*/
+#include<iostream>   
+using namespace std;
+int main(){
+          int a = 10;
+          int b = a--;    //post decrement.
+          cout<<"b is = " <<b <<endl;
+          cout<<"a is = " <<a;
+          return 0;
+}
