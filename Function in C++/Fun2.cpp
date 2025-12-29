2e4ed8415d83bccbe7d186f758e0239ee582a011
Pm Pranta <heyiampm@gmail.com>
2025-12-30 00:18:31 +0600

    DSA

---
+++ Function in C++/Fun2.cpp
@@ -0,0 +1,14 @@
+          #include <iostream>
+          using namespace std;
+
+          //Fuction 
+          int printpranta() {
+                    cout<<"Author-Pranta Mohajan\n";
+                    return 30;
+          }
+          int main() {
+                    int age =  printpranta();
+                    cout<<"My age is = " << age << endl;
+
+                    return 0;
+          }
