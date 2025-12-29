cac6f9e6c99f2dcd9f66923d7116b8c079fa8260
Pm Pranta <heyiampm@gmail.com>
2025-12-30 00:17:24 +0600

    DSA

---
+++ Function in C++/Fun1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//Fuction 
+void printpranta(){
+          cout<<"Author-Pranta Mohajan\n";
+}
+int main(){
+          printpranta();
+          printpranta();
+          printpranta();
+          printpranta();
+          return 0;
+}
