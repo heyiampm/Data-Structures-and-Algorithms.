63b4737a83b0ec40204cd0ed3e2c4e6ff6cb789f
Pm Pranta <heyiampm@gmail.com>
2025-12-30 00:22:56 +0600

    DSA

---
+++ Function in C++/Fun4.cpp
@@ -0,0 +1,12 @@
+// sum a two number using a function
+
+#include <iostream>
+using namespace std;
+          //Fuction 
+int sum(int a, int b){
+          int s = a + b;
+          return s;
+}
+int main(){
+          cout<<sum (20,20)<<endl;
+}
