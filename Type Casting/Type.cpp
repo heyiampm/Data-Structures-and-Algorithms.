3a69c02386339cb03829f1bb45706357eefce777
Pm Pranta <heyiampm@gmail.com>
2025-12-30 10:47:00 +0600

    DSA

---
+++ Type Casting/Type.cpp
@@ -0,0 +1,9 @@
+//📌 Type casting.
+#include<iostream>
+using namespace std;
+int main(){
+          double price  = 100.0;    //.
+          int new_price = price;  //this process is convert val.
+          cout<<new_price<<endl;
+          return 0;
+}
