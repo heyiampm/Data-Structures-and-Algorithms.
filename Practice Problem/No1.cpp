2d2268a5d4ec997c3136c59fbd6758ed273253cc
Pm Pranta <heyiampm@gmail.com>
2026-01-05 20:43:29 +0600

    DSA

---
+++ Practice Problem/No1.cpp
@@ -0,0 +1,15 @@
+//📌 input a two number total sum .
+
+#include<iostream>
+using namespace std;
+int main(){
+          int a,b;
+          cout<< "Enter a value: "<<endl;
+          cin>>a;
+
+          cout<< "Enter b value: " <<endl;
+          cin>>b;
+
+          cout<<"Total Sum is : " << (a+b)<<endl;
+          return 0;
+}
