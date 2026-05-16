7995f552583aa15cbbfefb57ea36feb1de394380
Pm Pranta <heyiampm@gmail.com>
2026-05-16 23:54:51 +0600

    feat: commit  on

--- Hackerank Problem solving/Problem 4.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    arr = map(int, input().split())
-    
-    uniqe_scores = set(arr)
-    list_scores = list(uniqe_scores)
-
-    list_scores.sort()
-    print(list_scores[-2])
